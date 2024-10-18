39656139dfccc02e5b74e913e98ee1256e21b269
Chugani05 <chugani205@gmail.com>
2024-10-18 20:13:14 +0100

    update

--- tasks/forms.py
+++ tasks/forms.py
@@ -6,7 +6,7 @@ from .models import Task
 class AddPostForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ('title', 'description', 'deadline')
+        fields = ('title', 'description', 'deadline', 'check_box')
         widgets = {
             'deadline': forms.DateInput(attrs={'type': 'date'})
         }
--- tasks/urls.py
+++ tasks/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.task_list, name='task-list'),
     path('done/', views.done_task, name='done_task'),
     path('pending/', views.pending_task, name='pending_task'),
-    path('t/<task_slug>/', views.task_detail, name='task-detail'),
+    path('task/<task_slug>/', views.task_detail, name='task-detail'),
     path('add-task/', views.add_task, name='add-task')
 ]
